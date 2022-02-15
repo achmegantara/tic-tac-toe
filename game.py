439341a8bc2cf50906256ce245ca5780b6e95a4a
Achmad Akbar Megantara <achmegantara@gmail.com>
2022-02-15 18:57:53 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,34 @@
+# credit Github: https://www.github.com/kying18
+
 import math
 import time
+from player import HumanPlyaer, RandomComputerPlayer, SmartComputerPlayer
+
+
+class TicTacToe():
+    def __init__(self):
+        self.board = self.make_board()
+        self.current_winner = None
+
+    @staticmethod
+    def make_board():
+        return[' ' for _ in range(9)]
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)]
+                        for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def make_move(self, square, letter):
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
